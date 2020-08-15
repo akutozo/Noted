669dc8a55f1bdbc0b64f17a7df614203cca29ddc
Jacob Daughtry <akutozo@gmail.com>
2020-08-15 14:53:14 -0500

    Added initial Readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Note Taker Starter Code
+# Noted
+
+An application to make notes, take notes and forsake notes. Ideal for school, meetings, remembering birthdays and why your aunt Susan called at 3:00 AM in the morning to talk about the get and post routes for a zookeepr website.
+
+## Built With:
+
+* HTML
+* CSS
+* Javascript
+ 
+ ## Tested with
+
+ * Express
+ * Jest
+
+ ## Built By
+
+ Jacob Daughtry
+
+ ## Additonal Support Provided By
+
+ Grant Emerson
+
+ ## Sponsored By
+
+The founders of https://www.emojicode.org/
+The minds behind http://sixdegreesofkanyewest.com/
--- server.js
+++ server.js
@@ -3,20 +3,25 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const htmlRoutes = require('./routes/htmlRoutes');
+
 // Setup Port for Heroku, Start Express
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// parse incoming string or array data
+// Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 
-// parse incoming JSON data
+// Parse incoming JSON data
 app.use(express.json());
 
-//Let's make it public
+// Client Navigation Routes
+app.use('/', htmlRoutes);
+
+// Let's make it public
 app.use(express.static('public'));
 
-//Allow Express to Listen
+// Allow Express to Listen
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
